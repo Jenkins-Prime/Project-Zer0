a8f92969c1124083ca8bfcc1192d094c74a7c269
joshmond <joshmond@ymail.com>
2015-09-23 04:31:52 +0100

    Updated character controller, character can now rotate in the air and I've added an air control variable to the inspector to give our character a bit of velocity and control while in the air.

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -1,151 +1,187 @@
 using UnityEngine;
 using System.Collections;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : MonoBehaviour 
 {
-	private float turnSmoothing;   
-	private float speedDampTime;  
-	private float moveSpeed;
-	private float jumpVelocity;
-	private float slopeLimit;
+	public float speed;
+	public float gravity;
+	public float maxVelocityChange;
+	public float jumpHeight;
+	public float airControl;
+
+	[SerializeField]
+	private bool grounded;
 	private bool isCrouching;
-	private bool isGrounded;
-	private Vector3 targetDirection;
-	private Vector3 targetMoveForward;
+	[SerializeField]
+	private bool isJumping;
+	private bool isInAir;
+	private Vector3 targetVelocity;
+	private float turnSpeed;
+	private float slopeLimit;
+	private int jumpTime;
+	private int timeOnGround;
+	private Vector3 velocity;
+	private Vector3 velocityChange;
 
-
-
-	private Animator animator;                   
-	private Rigidbody rb;
-	private Transform cameraTransform;
-
-
-
-	void Awake ()
+	private Rigidbody rBody;
+	private Animator animator;
+	
+	
+	
+	void Awake () 
 	{
-		animator = GetComponent<Animator>();
-		rb = GetComponent<Rigidbody> ();
-		cameraTransform = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+		rBody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator> ();
+		rBody.freezeRotation = true;
+		rBody.useGravity = false;
 	}
 
 	void Start()
 	{
-		moveSpeed = 200.0f;
-		turnSmoothing = 15.0f;
-		speedDampTime = 0.1f;
+		speed = 3.0f;
+		gravity = 10.0f;
+		maxVelocityChange = 10.0f;
+		jumpHeight = 2.0f;
+		airControl = 3.0f;
+		turnSpeed = 50.0f;
 		slopeLimit = 50.0f;
-		jumpVelocity = 250.0f;
+		timeOnGround = 1;
+		grounded = false;
 		isCrouching = false;
-		isGrounded = false;
-
+		isJumping = false;
 	}
 
 	void Update()
 	{
-		Crouch ();
-	}
-
-	void FixedUpdate ()
-	{
-		float horizontal = Input.GetAxis("Horizontal");
-		float vertical = Input.GetAxis("Vertical");
-		Move(horizontal, vertical);
-		Jump ();
+		Crouch();
 	}
 	
-	void Move(float horizontal, float vertical)
+	void FixedUpdate () 
 	{
-		if(horizontal != 0f || vertical != 0f)
+		MovePlayer ();
+		Jump ();
+	}
+
+	private void MovePlayer()
+	{
+		if (grounded) 
 		{
-			Rotate(horizontal, vertical);
-			CheckMovement(horizontal, vertical);
-			animator.SetFloat("speed", 1.0f);
+			animator.SetBool("onGround", true);
+			targetVelocity = new Vector3 (Input.GetAxis ("Horizontal") * Time.deltaTime, 0, Input.GetAxis ("Vertical") * Time.deltaTime).normalized;
+			targetVelocity *= speed;
+
+
+			// Apply a force that attempts to reach our target velocity
+			velocity = rBody.velocity;
+			velocityChange = (targetVelocity - velocity);
+			velocityChange.x = Mathf.Clamp (velocityChange.x, -maxVelocityChange, maxVelocityChange);
+			velocityChange.z = Mathf.Clamp (velocityChange.z, -maxVelocityChange, maxVelocityChange);
+			velocityChange.y = 0;
+			rBody.AddForce (velocityChange, ForceMode.VelocityChange);
+			isJumping = true;
+			Rotate (targetVelocity.x, targetVelocity.z);
+
+			if(targetVelocity != Vector3.zero)
+			{
+				animator.SetFloat("speed", 5.0f);
+			}
+			else
+			{
+				animator.SetFloat("speed", 0.0f);
+			}
 		}
 		else
 		{
+			float h = Input.GetAxis("Horizontal");
+			float v = Input.GetAxis("Vertical");
+
+			if(h != 0 || v != 0)
+			{
+				targetVelocity = new Vector3(h, 0.0f, v);
+				targetVelocity = Camera.main.transform.TransformDirection (targetVelocity);
+				Quaternion targetRotation = Quaternion.LookRotation(targetVelocity);
+				Quaternion newRotation = Quaternion.Lerp(rBody.rotation, targetRotation, turnSpeed * Time.deltaTime);
+				rBody.MoveRotation(newRotation);
+				rBody.AddForce(new Vector3 (h * airControl, 0.0f, v * airControl));
+			}
+
 			animator.SetFloat("speed", 0.0f);
+
 		}
-			
+
 	}
 
-	private void Rotate(float horizontal, float vertical)
-	{
-		targetDirection = new Vector3(horizontal, 0.0f, vertical);
-		targetDirection = Camera.main.transform.TransformDirection (targetDirection);
-		Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-		Quaternion newRotation = Quaternion.Lerp(rb.rotation, targetRotation, turnSmoothing * Time.deltaTime);
-		Vector3 offsetRotation = new Vector3 (0.0f, newRotation.eulerAngles.y, 0.0f);
-		newRotation.eulerAngles = offsetRotation;
-		rb.MoveRotation(newRotation);
-	}
-
-	private void CheckMovement(float horizontal, float vertical)
-	{
-		if(isGrounded)
-		{
-			targetMoveForward = Camera.main.transform.TransformDirection(Vector3.forward);
-			Debug.Log(targetMoveForward);
-			if(vertical < 0)
-			{
-				rb.velocity = -transform.forward * vertical * moveSpeed * Time.fixedDeltaTime;
-			}
-			
-			if(vertical > 0)
-			{
-				rb.velocity = targetMoveForward * vertical * moveSpeed * Time.fixedDeltaTime;
-			}
-			
-			if(horizontal < 0)
-			{
-				rb.velocity = -transform.forward * horizontal * moveSpeed * Time.fixedDeltaTime;
-			}
-			
-			if(horizontal > 0)
-			{
-				rb.velocity = transform.forward * horizontal * moveSpeed * Time.fixedDeltaTime;
-			}
-		}
-	}
 
 
 	private void Crouch()
 	{
-		if(Input.GetButton("Crouch") && isGrounded)
+		if(Input.GetButton("Crouch") && grounded)
 		{	
 			isCrouching = true;
 			animator.SetBool ("Crouching", true);
-
 		}
-
-		if(Input.GetButtonUp("Crouch") && isGrounded)
+		
+		if(Input.GetButtonUp("Crouch") && grounded)
 		{
 			isCrouching = false;
 			animator.SetBool ("Crouching", false);
 		}
 	}
-	
-	private void Jump()
+
+
+
+	private void Rotate(float horizontal, float vertical)
 	{
-		if(Input.GetButtonDown("Jump") && isGrounded)
+		if(targetVelocity != Vector3.zero)
 		{
-			rb.AddForce(0, jumpVelocity, 0);
+			targetVelocity = new Vector3(horizontal, 0.0f, vertical).normalized;
+			targetVelocity = Camera.main.transform.TransformDirection (targetVelocity);
+			Quaternion targetRotation = Quaternion.LookRotation(targetVelocity);
+			Quaternion newRotation = Quaternion.Lerp(rBody.rotation, targetRotation, turnSpeed * Time.deltaTime);
+			rBody.MoveRotation(newRotation);
 		}
 	}
 
+
+
+	private void Jump()
+	{
+		if (Input.GetButtonDown("Jump") && isJumping) 
+		{
+			rBody.velocity = new Vector3 (0, CalculateJumpVerticalSpeed (), 0);
+			isJumping = false;
+		}
+		else
+		{
+			rBody.AddForce(new Vector3 (0, -gravity * rBody.mass, 0));
+		}
+
+		
+
+	}
+	
 	void OnCollisionStay(Collision collision)
 	{
 		foreach(ContactPoint contact in collision.contacts)
 		{
 			if(Vector3.Angle(contact.normal, Vector3.up) < slopeLimit)
 			{
-				isGrounded = true;
+				grounded = true;
 			}
 		}
 	}
-	
+
 	void OnCollisionExit()
 	{
-		isGrounded = false;
+		grounded = false;
+		isInAir = true;
 	}
 
+	
+	float CalculateJumpVerticalSpeed () 
+	{
+
+		return Mathf.Sqrt(2 * jumpHeight * gravity);
+	}
 }
+
